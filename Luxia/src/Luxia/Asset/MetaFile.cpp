991f3988ef240c0bd7cd5e070ccb471dab2c53a5
Guassy <haaglgw@gmail.com>
2025-11-25 17:38:41 +0100

    MetaFile class holds previous assetfile class stuff

--- Luxia/src/Luxia/Asset/MetaFile.cpp
+++ Luxia/src/Luxia/Asset/MetaFile.cpp
@@ -4,5 +4,96 @@
 
 
 namespace Luxia::Assets {
+	bool MetaFile::Create(const std::filesystem::path& m_srcPath, const std::filesystem::path& m_relativePath, const std::filesystem::path& m_metaPath, const std::string& m_name, const AssetType& m_type) {
+		type = m_type;
+		srcPath = m_srcPath.lexically_normal();
+		relPath = m_relativePath.lexically_normal();
+		metaPath = m_metaPath.lexically_normal();
+		extension = m_srcPath.extension().string();
+		name = m_name;
 
+		// Save 
+		loaded = Save();;
+
+		return loaded;
+	}
+
+	// Should be YAML!
+	bool MetaFile::Load(const std::filesystem::path& m_metaPath) {
+		// Open file
+		metaPath = m_metaPath;
+		std::ifstream infile(metaPath, std::ios::in);
+
+		// Throw runtime error
+		if (!infile.is_open()) {
+			LX_CORE_ERROR("Failed to open metafile: {}", metaPath.string());
+			return false;
+		}
+
+		int success = 0;
+		std::string line;
+		while (std::getline(infile, line)) {
+			// Trim whitespace if needed
+			if (line.rfind("type=", 0) == 0) {
+				uint64_t val = std::stoull(line.substr(5));
+				type = static_cast<Luxia::AssetType>(val);
+				success++;
+			}
+			else if (line.rfind("guid=", 0) == 0) {
+				guid = std::stoull(line.substr(5));
+				success++;
+			}
+			else if (line.rfind("path=", 0) == 0) {
+				srcPath = line.substr(5);
+				success++;
+			}
+			else if (line.rfind("rel_path=", 0) == 0) {
+				relPath = line.substr(9);
+				success++;
+			}
+			else if (line.rfind("name=", 0) == 0) {
+				name = line.substr(5);
+				success++;
+			}
+			else if (line.rfind("extn=", 0) == 0) {
+				extension = line.substr(5);
+				success++;
+			}
+		}
+
+		infile.close();
+
+		// if all lines werent found, return false
+		if (success != 6) { return false; }
+
+		loaded = true;
+		return loaded;
+	}
+
+	// Should be YAML!
+	bool MetaFile::Save() {
+		// Open file
+		std::ofstream outfile(metaPath);
+		if (!outfile.is_open()) {
+			LX_CORE_ERROR("Failed to save metafile: {}", metaPath.string());
+			return false;
+		}
+
+		// outfile << out.c_str();
+		outfile << "type=" << (int)type << "\n";
+		outfile << "guid=" << (uint64_t)guid << "\n";
+		outfile << "path=" << srcPath.string() << "\n";
+		outfile << "rel_path=" << relPath.string() << "\n";
+		outfile << "name=" << name << "\n";
+		outfile << "extn=" << extension << "\n";
+
+		outfile.close();
+
+		return true;
+	}
+
+	void MetaFile::Unload() {
+		loaded = false;
+
+	}
 }
--- Luxia/src/Luxia/Asset/MetaFile.h
+++ Luxia/src/Luxia/Asset/MetaFile.h
@@ -1,8 +1,30 @@
 #pragma once
 
-namespace Luxia::Assets {
-	class MetaFile
-	{
+#include "Luxia/Core/Core.h"
+#include "Luxia/Asset/AssetFile.h"
 
+namespace Luxia::Assets {
+	class LUXIA_API MetaFile
+	{
+		MetaFile() = default;
+		~MetaFile() = default;
+
+		bool Create(const std::filesystem::path& m_srcPath, const std::filesystem::path& m_relativePath, const std::filesystem::path& m_metaPath, const std::string& m_name, const AssetType& m_type); // Create the metafile from a source
+		bool Load(const std::filesystem::path& m_metaPath);
+		bool Save();
+		void Unload(); // Unload the asset
+
+		AssetType type = AssetType::NoType;
+
+		std::string extension = ".none"; // .png, .obj, etc. Helps to decide how to load
+		std::filesystem::path srcPath = "no/path";
+		std::filesystem::path relPath = "no/path";
+		std::filesystem::path metaPath = "no/path";
+		std::string name = "no_name";
+
+
+		bool loaded = false;
+
+		GUID guid;
 	};
 }
