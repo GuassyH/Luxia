92912c4854857e64e7b1864f08899dc74a404ea0
Guassy <haaglgw@gmail.com>
2025-12-19 17:13:50 +0100

    Asset / Rendering creation restructure, was going to use Macros but due to risk of problems i wont for now

--- Luxia/src/Luxia/Platform/AssetCreation.h
+++ Luxia/src/Luxia/Platform/AssetCreation.h
@@ -7,6 +7,8 @@
 #include "Luxia/Rendering/API/ICamera.h"
 #include "Luxia/Rendering/API/IShader.h"
 #include "Luxia/Rendering/API/IMaterial.h"
+#include "Luxia/Events/EventHandler.h"
+
 
 #ifdef LUXIA_RENDERER_OPENGL
 	#include "Luxia/Platform/OpenGL/GL_Window.h"
@@ -15,89 +17,37 @@
 	#include "Luxia/Platform/OpenGL/GL_Camera.h"
 	#include "Luxia/Platform/OpenGL/GL_Shader.h"
 	#include "Luxia/Platform/OpenGL/GL_Material.h"
-#endif
 
-#ifdef LUXIA_RENDERER_VULKAN
-	#error Vulkan Renderer not yet supported in Luxia!
-#endif
+namespace Luxia::Platform::Assets {
+	inline std::shared_ptr<ICamera> LUXIA_API CreateCamera(auto&&... args) {
+		return std::make_shared<Platform::OpenGL::GL_Camera>(std::forward<decltype(args)>(args)...);
+	}
 
-
-namespace Luxia::Platform::Assets
-{
-	// Create the window, with width, height, and title, this will change automatically based on the OS
-	inline std::shared_ptr<Luxia::IWindow> CreateAppWindow(int width, int height, std::string title, std::shared_ptr<EventHandler> handler)
-	{
-		std::shared_ptr<Luxia::IWindow> win;
-
-		#ifdef LUXIA_RENDERER_OPENGL
-			win = std::make_shared<Luxia::Platform::OpenGL::GL_Window>();
-		#else 
-			#error Luxia doesnt support your Renderer!
-		#endif
-
-		win->SetHandler(handler);
-		win->Create(1920, 1080, "Luxia Application");
+	inline std::shared_ptr<IWindow> LUXIA_API CreateAppWindow(int width, int height, std::string name, std::shared_ptr<Luxia::EventHandler> m_eventHandler) {
+		auto win = std::make_shared<Platform::OpenGL::GL_Window>();
+		win->SetHandler(m_eventHandler);
+		win->Create(width, height, name);
 		return win;
 	}
 
-	inline std::shared_ptr<Luxia::ITexture> CreateTexture() {
-		std::shared_ptr<Luxia::ITexture> tex;
-
-		#ifdef LUXIA_RENDERER_OPENGL
-			tex = std::make_shared<Luxia::Platform::OpenGL::GL_Texture>();
-		#else 
-			#error Luxia doesnt support your Renderer!
-		#endif
-		return tex;
+	inline std::shared_ptr<ITexture> LUXIA_API CreateTexture() {
+		return std::make_shared<Platform::OpenGL::GL_Texture>();
 	}
 
-	inline std::shared_ptr<Luxia::IModel> CreateModel() {
-		std::shared_ptr<Luxia::IModel> mod;
-
-		#ifdef LUXIA_RENDERER_OPENGL
-			mod = std::make_shared<Luxia::Platform::OpenGL::GL_Model>();
-		#else 
-			#error Luxia doesnt support your Renderer!
-		#endif
-
-		return mod;
+	inline std::shared_ptr<IModel> LUXIA_API CreateModel() {
+		return std::make_shared<Platform::OpenGL::GL_Model>();
 	}
 
-	inline std::shared_ptr<Luxia::ICamera> CreateCamera(int width, int height) {
-		std::shared_ptr<Luxia::ICamera> cam;
-
-		#ifdef LUXIA_RENDERER_OPENGL
-				cam = std::make_shared<Luxia::Platform::OpenGL::GL_Camera>(width, height);
-		#else 
-			#error Luxia doesnt support your Renderer!
-		#endif
-
-		return cam;
+	inline std::shared_ptr<IMaterial> LUXIA_API CreateMaterial(auto&&... args) {
+		return std::make_shared<Platform::OpenGL::GL_Material>(std::forward<decltype(args)>(args)...);
 	}
 
-
-	inline std::shared_ptr<Luxia::IShader> CreateShader(const char* fragSrc, const char* vertSrc) {
-		std::shared_ptr<Luxia::IShader> shader;
-
-		#ifdef LUXIA_RENDERER_OPENGL
-				shader = std::make_shared<Luxia::Platform::OpenGL::GL_Shader>(fragSrc, vertSrc);
-		#else 
-		#error Luxia doesnt support your Renderer!
-		#endif
-
-		return shader;
-	}
-
-	template<typename... Args>
-	inline std::shared_ptr<Luxia::IMaterial> CreateMaterial(Args&&... args) {
-		std::shared_ptr<Luxia::IMaterial> mat;
-
-		#ifdef LUXIA_RENDERER_OPENGL
-			mat = std::make_shared<Luxia::Platform::OpenGL::GL_Material>(std::forward<Args>(args)...);
-		#else 
-			#error Luxia doesnt support your Renderer!
-		#endif
-
-		return mat;
+	inline std::shared_ptr<IShader> LUXIA_API CreateShader(auto&&... args) {
+		return std::make_shared<Platform::OpenGL::GL_Shader>(std::forward<decltype(args)>(args)...);
 	}
 }
+
+#elif LUXIA_RENDERER_VULKAN
+	#error Vulkan Renderer not yet supported in Luxia!
+#endif
+
--- Luxia/src/Luxia/Rendering/RenderingCreation.h
+++ Luxia/src/Luxia/Rendering/RenderingCreation.h
@@ -3,53 +3,31 @@
 #include "Luxia/Core/Core.h"
 
 #include "Luxia/Rendering/API/IRenderer.h"
-#include "Luxia/Rendering/OpenGL/OpenGL_Renderer.h"
-
 #include "Luxia/Rendering/API/IRenderBuffers.h"
+
+
+#ifdef LUXIA_RENDERER_OPENGL
+
+#include "Luxia/Rendering/OpenGL/OpenGL_Renderer.h"
 #include "Luxia/Rendering/OpenGL/OpenGLRenderBuffers.h"
 
 namespace Luxia::Platform::Rendering {
-
 	inline std::shared_ptr<Luxia::Rendering::IRenderer> LUXIA_API CreateRenderer() {
-		std::shared_ptr<Luxia::Rendering::IRenderer> renderer;
-		#ifdef LUXIA_RENDERER_OPENGL
-			renderer = std::make_shared<Luxia::Rendering::OpenGL::OpenGL_Renderer>();
-		#else
-			#error Luxia doesnt support your renderer!
-		#endif // LUXIA_RENDERER_OPENGL
-		return renderer;
+		return std::make_shared<Luxia::Rendering::OpenGL::OpenGL_Renderer>();
 	}
-
 	inline std::shared_ptr<Luxia::Rendering::Buffers::VAO> LUXIA_API CreateVAO() {
-		std::shared_ptr<Luxia::Rendering::Buffers::VAO> vao;
-		#ifdef LUXIA_RENDERER_OPENGL
-			vao = std::make_shared<Luxia::Rendering::OpenGL::Buffers::OpenGL_VAO>();
-		#else
-			#error Luxia doesnt support your renderer!		
-		#endif // LUXIA_RENDERER_OPENGL
-
-		return vao;
+		return std::make_shared<Luxia::Rendering::OpenGL::Buffers::OpenGL_VAO>();
 	}
-
 	inline std::shared_ptr<Luxia::Rendering::Buffers::VBO> LUXIA_API CreateVBO() {
-		std::shared_ptr<Luxia::Rendering::Buffers::VBO> vbo;
-		#ifdef LUXIA_RENDERER_OPENGL
-			vbo = std::make_shared<Luxia::Rendering::OpenGL::Buffers::OpenGL_VBO>();
-		#else
-			#error Luxia doesnt support your renderer!
-		#endif // LUXIA_RENDERER_OPENGL
-
-		return vbo;
+		return std::make_shared<Luxia::Rendering::OpenGL::Buffers::OpenGL_VBO>();
 	}
 
 	inline std::shared_ptr<Luxia::Rendering::Buffers::EBO> LUXIA_API CreateEBO() {
-		std::shared_ptr<Luxia::Rendering::Buffers::EBO> ebo;
-		#ifdef LUXIA_RENDERER_OPENGL
-			ebo = std::make_shared<Luxia::Rendering::OpenGL::Buffers::OpenGL_EBO>();
-		#else
-			#error Luxia doesnt support your renderer!
-		#endif // LUXIA_RENDERER_OPENGL
-
-		return ebo;
+		return std::make_shared<Luxia::Rendering::OpenGL::Buffers::OpenGL_EBO>();
 	}
 }
+
+#elif LUXIA_RENDERER_VULKAN
+#error Vulkan Renderer not yet supported in Luxia!
+#endif
+
