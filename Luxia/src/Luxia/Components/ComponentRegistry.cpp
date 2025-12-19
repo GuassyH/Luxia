5f19b3b4a81f119a8041ba962abbf76a2d4e5f00
Guassy <haaglgw@gmail.com>
2025-12-19 16:45:51 +0100

    Component Registry Optimization

--- Luxia/src/Luxia/Components/ComponentRegistry.cpp
+++ Luxia/src/Luxia/Components/ComponentRegistry.cpp
@@ -5,14 +5,11 @@
 namespace Luxia {
 
     std::vector<ComponentInfo> componentRegistry = {
-    { "Camera Component",
-      [](Components::Transform* t) { return t->HasComponent<Components::Camera>(); },
-      [](Components::Transform* t) { t->AddComponent<Components::Camera>(1920, 1080); }
-    },
-    { "Mesh Renderer Component",
-      [](Components::Transform* t) { return t->HasComponent<Components::MeshRenderer>(); },
-      [](Components::Transform* t) { t->AddComponent<Components::MeshRenderer>(); }
-    },
+ 
+        LX_CREATE_COMPONENT_INFO(Components::Camera, "Camera", 1920, 1080),
+        LX_CREATE_COMPONENT_INFO(Components::MeshRenderer, "Mesh Renderer"),
         // Add more here
     };
 };
+
+
--- Luxia/src/Luxia/Components/ComponentRegistry.h
+++ Luxia/src/Luxia/Components/ComponentRegistry.h
@@ -16,4 +16,17 @@ namespace Luxia {
 	};
 
 	extern std::vector<ComponentInfo> componentRegistry;
+
 };
+
+#define LX_REGISTER_COMPONENT(TYPE, COMP_NAME, ...) \
+    ::Luxia::componentRegistry.push_back({ COMP_NAME, \
+        [](Components::Transform* t){ return t->HasComponent<TYPE>(); }, \
+        [](Components::Transform* t){ t->AddComponent<TYPE>(__VA_ARGS__); } \
+    });
+
+#define LX_CREATE_COMPONENT_INFO(TYPE, COMP_NAME, ...) \
+    ::Luxia::ComponentInfo{ COMP_NAME, \
+        [](Components::Transform* t){ return t->HasComponent<TYPE>(); }, \
+        [](Components::Transform* t){ t->AddComponent<TYPE>(__VA_ARGS__); } \
+    }
